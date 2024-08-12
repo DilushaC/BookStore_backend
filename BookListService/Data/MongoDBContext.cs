35fcb365fd9f231ae1e950ec13502a5f80b88390
Dilusha <dilushafernando97@gmail.com>
2024-08-12 18:40:32 +0530

    MongoDB service added to booklist project

---
+++ BookListService/Data/MongoDBContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using BookListService.Models;
+
+namespace BookListService.Data
+{
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("MongoDB:ConnectionString"));
+            _database = client.GetDatabase(configuration.GetValue<string>("MongoDB:DatabaseName"));
+        }
+
+        public IMongoCollection<Book> Books => _database.GetCollection<Book>("Books");
+    }
+}
---
+++ BookListService/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookListService.Models
+{
+    public class Book
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
